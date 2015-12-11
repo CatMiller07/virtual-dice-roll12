06367e28f918232bfb59b07b329a654ae4d3dd8a
catmiller07 <catmiller07@comcast.net>
2015-12-10 21:14:18 -0500

    added animation
    added 2 images: a spinning gif and a static png.

--- index.js
+++ index.js
@@ -7,28 +7,58 @@ function dieRoll(){
 	 
  }  
 
+function displayDiceRoll(iDice,iDieFace){
+	
+   	iDieFace.innerHTML = "";
+	
+    //console.log("here i am");
+	
+   iDice.src = "Dodecahedron.gif";	
+   setTimeout(function(){
+	    iDice.src = "dodecahedron-frame1.png";
+    	iDieFace.innerHTML = dieRoll();     			
+		},1000);
+   
+	
+       	
+}
 var virtDie0 = document.getElementById("anim-die0");
+var dieFace0 = document.getElementById("roll-val0");
 var virtDie1 = document.getElementById("anim-die1");
+var dieFace1 = document.getElementById("roll-val1");
 var virtDie2 = document.getElementById("anim-die2");
+var dieFace2 = document.getElementById("roll-val2");
 var virtDie3 = document.getElementById("anim-die3");
-
-virtDie0.onclick=function()
-{   var dieFace = document.getElementById("roll-val0");
-    	dieFace.innerHTML = dieRoll();
-
+var dieFace3 = document.getElementById("roll-val3");
+/*****
+virtDie0.onclick=function(){   
+	var dieFace = document.getElementById("roll-val0");
+   console.log("here i am");
+    virtDie0.src = "Dodecahedron.gif";	
+   setTimeout(function(){
+	      virtDie0.src = "dodecahedron-frame1.png";
+    	dieFace.innerHTML = dieRoll();     			
+		},1000);
 }
-virtDie1.onclick=function()
-{   var dieFace = document.getElementById("roll-val1");
-    	dieFace.innerHTML = dieRoll();
+ 
+ ****/  
+   
+virtDie0.onclick=function(){
+	
+	displayDiceRoll(virtDie0,dieFace0); 
+};
+	 
+     
+virtDie1.onclick=function(){
+	displayDiceRoll(virtDie1,dieFace1); 
 
-}
-virtDie2.onclick=function()
-{   var dieFace = document.getElementById("roll-val2");
-    	dieFace.innerHTML = dieRoll();
+};
 
-}
-virtDie3.onclick=function()
-{   var dieFace = document.getElementById("roll-val3");
-    	dieFace.innerHTML = dieRoll();
+virtDie2.onclick=function() {
+	displayDiceRoll(virtDie2,dieFace2);
 
+};
+virtDie3.onclick=function(){
+	displayDiceRoll(virtDie3,dieFace3);
 }
+
